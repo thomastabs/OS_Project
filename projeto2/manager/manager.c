af7e38ae40f47be1f2c7cc71eb1e3a8d0835e2ae
Fábio Sobrinho <fabiosobrinho350@gmail.com>
2023-01-06 10:25:54 +0000

    manager e mbroker

--- projeto2/manager/manager.c
+++ projeto2/manager/manager.c
@@ -2,15 +2,23 @@
 
 static void print_usage() {
     fprintf(stderr, "usage: \n"
-                    "   manager <register_pipe_name> create <box_name>\n"
-                    "   manager <register_pipe_name> remove <box_name>\n"
-                    "   manager <register_pipe_name> list\n");
+                    "   manager <register_pipe> <pipe_name> create <box_name>\n"
+                    "   manager <register_pipe> <pipe_name> remove <box_name>\n"
+                    "   manager <register_pipe> <pipe_name> list\n");
 }
 
 int main(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
-    print_usage();
+    if(argc < 4){
+        print_usage();
+    }
+
+    char *server_pipe = argv[1];
+    char *pipe_name = argv[2];
+    char *type_command = argv[3];
+    char *box_name = argv[4];
+
+    
+    
     WARN("unimplemented"); // TODO: implement
     return -1;
 }
--- projeto2/mbroker/mbroker.c
+++ projeto2/mbroker/mbroker.c
@@ -1,9 +1,70 @@
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include "logging.h"
+#include "fs/operations.h"
+
+typedef enum {PUB, SUB} client_type;
+
+typedef struct {
+    char session_name;
+    int pipe;
+    client_type client;
+} Session;
 
 int main(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
-    fprintf(stderr, "usage: mbroker <pipename>\n");
-    WARN("unimplemented"); // TODO: implement
+    if(argc < 2){
+        fprintf(stderr, "usage: mbroker <pipename> <max_sessions>\n");
+    }
+    
+    char *pipe_name = argv[1];
+    int max_sessions = atoi(argv[2]);
+    Session sessions[max_sessions]; 
+
+    if(tfs_init(NULL) != -1){
+        fprintf(stderr, "Não foi possivel começar o servidor\n");
+        return -1;
+    }    
+    		
+            //signal(SIGPIPE, SIG_IGN);
+
+    printf("[INFO]: Starting TecnicoFS server with pipe called %s with %d sessions\n", pipe_name, max_sessions);
+
+    // remove pipe if it does exist
+    if (unlink(pipe_name) != 0 && errno != ENOENT) {
+        fprintf(stderr, "[ERR]: unlink(%s) failed: %s\n", pipe_name,
+                strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    // create pipe
+    if (mkfifo(pipe_name, 0640) != 0 && errno != EEXIST) {
+        fprintf(stderr, "[ERR]: mkfifo failed: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    // server request
+    int server_pipe = open(pipe_name, O_WRONLY);
+    if (server_pipe == -1){
+        fprintf(stderr, "[ERR]: open failed: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    start_sessions();
+    
+
+
+
+
+    close(server_pipe);
+    tfs_unlink(pipe_name);
     return -1;
 }
