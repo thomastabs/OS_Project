e7f1da9118b54523afaaca2163da3ad4b2e0669d
thomastabs <tomassantostaborda@gmail.com>
2023-01-10 14:21:20 +0000

    pub completo falta vdc acho

--- projeto2/publisher/pub.c
+++ projeto2/publisher/pub.c
@@ -1,4 +1,4 @@
-#include "logging.h"
+#include "../utils/logging.h"
 #include "../utils/common.h"
 #include "operations.h"
 #include <assert.h>
@@ -15,6 +15,7 @@
 
 #define MAX_PIPE_NAME 256
 #define MAX_BOX_NAME 32
+#define MAX_MSG_SIZE 1024   
 
 // Helper function to send pub request
 void send_pub_request(int server_pipe, char* client_pipe, char* box) {
@@ -39,6 +40,20 @@ void send_pub_request(int server_pipe, char* client_pipe, char* box) {
     return response;
 }
 
+void send_pub_msg(int session_pipe, char *msg){
+    char pub_msg[sizeof(uint8_t) +  MAX_MSG_SIZE * sizeof(char)];
+    uint8_t op_code = SEND_MESSAGE;
+    memcpy(pub_msg, &op_code, sizeof(uint8_t));
+    memset(pub_msg + 1, '\0', MAX_MSG_SIZE * sizeof(char));
+    memcpy(pub_msg + 1, msg, strlen(msg) * sizeof(char));
+
+    // write to the the client session pipe
+    if (write(session_pipe, pub_msg, strlen(pub_msg)) == -1){
+        return -1;
+    }
+    return 0;
+}
+
 int main(int argc, char **argv) {
     char *register_pipename = argv[1]; 
     char *pub_pipename = argv[2];
@@ -60,15 +75,46 @@ int main(int argc, char **argv) {
         exit(EXIT_FAILURE);
     }
 
-    // Open pipe for writing
+    // Open session pipe for writing
     // This waits for someone to open it for reading
-    int tx = open(register_pipename, O_WRONLY);
-    if (tx == -1) {
+    int server_pipe = open(server_pipe, O_WRONLY);
+    if (server_pipe == -1) {
+        fprintf(stderr, "[ERR]: open failed: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    
+    send_pub_request(register_pipename, pub_pipename, box_name);
+    
+    //se for aceite faz o resto
+
+    // Open pipe for writing
+    int session_pipe = open(pub_pipename, O_WRONLY);
+    if (session_pipe == -1) {
         fprintf(stderr, "[ERR]: open failed: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    send_pub_request(register_pipename, pub_pipename, box_name);
+    char input[MAX_MSG_SIZE];
+	memset(input, 0, MAX_MSG_SIZE);
+	int help = 0;
+
+	while (fgets(input, MAX_MSG_SIZE, stdin)) {;
+		/* if line too long, truncate and swallow the rest of the line */
+		if (strlen(input) >= MAX_MSG_SIZE - 1) {
+			input[MAX_MSG_SIZE - 1] = '\0';
+			while (getchar() != '\n' && !feof(stdin))
+				;
+		}
+
+		input[strcspn(input, "\n")] = 0;
+        send_pub_msg(session_pipe, input);
+        //depois de ser clicado enter, um input é preenchido e 
+        //assim vai para a proxima linha 
+	}
+
+    // when the EOF is pressed with the Crtl D, then the session pipe will be closed
+    fprintf(stderr, "[INFO]: closing session pipe\n");
+    close(session_pipe);
 
     return 0;
 }
